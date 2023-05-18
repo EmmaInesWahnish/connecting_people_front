355ab82bae9c08a59c86a5c8b2f88d1b84d176fe
Emma <emma.wahnish@groovinads.com>
2023-05-18 14:32:48 -0300

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,32 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+	extends: [
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"standard",
+		"eslint-config-prettier",
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react"],
+	rules: {
+		// values : warn - off - error
+		// "react/react-in-jsx-scope": "off"
+		"no-unused-vars": "warn",
+		"camelcase": "off",
+		"react/prop-types": "off",
+		"no-unneeded-ternary": "off",
+		"react/no-children-prop": "off"
+	},
+};
